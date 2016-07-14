9432c52cbc535e8ef00e0736a0e6657835264954
mauro.gonzalez <mauro.gonzalez@globallogic.com>
2016-07-14 18:20:34 -0300

    added variables on used ids, change delete to truncate

--- db/002.Data.sql
+++ db/002.Data.sql
@@ -8,18 +8,30 @@ END
 GO
 
 IF EXISTS(SELECT 1 FROM [fixture].[MatchType])
-BEGIN
-	--Deletes Teams with foreign key to MatchType, should probably remove all rows from table
-	DELETE FROM [fixture].[Match] WHERE [TypeId] IS NOT NULL
+BEGIN			
+	IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME ='FK_Match_MatchType')
+	BEGIN
+		--Truncates Match table to safely truncate MatchType table
+		TRUNCATE TABLE [fixture].[Match]
 
-	--Para truncar la tabla MatchType tengo que dropear las constraints y volverlas a generar, 
-	--o hacer un ALTER SWITCH a otra tabla con la misma estructura pero sin constraints
-	DELETE FROM [fixture].[MatchType]
+		--Drops foreign key constraint that references MatchType
+		ALTER TABLE [fixture].[Match]
+		DROP CONSTRAINT [FK_Match_MatchType]
+
+		--Truncates Table
+		TRUNCATE TABLE [fixture].[MatchType]
+
+		--Recreates foreign key constraint
+		ALTER TABLE [fixture].[Match]  WITH CHECK ADD CONSTRAINT [FK_Match_MatchType] FOREIGN KEY([TypeId])
+		REFERENCES [fixture].[MatchType] ([Id])
+	END
 END
 	INSERT INTO [fixture].[MatchType] ([Name])
 	VALUES ('Cuartos de Final')
+
 	INSERT INTO [fixture].[MatchType] ([Name])
 	VALUES ('Semifinal')
+
 	INSERT INTO [fixture].[MatchType] ([Name])
 	VALUES ('Final')
 GO
@@ -53,48 +65,59 @@ BEGIN
 END
 GO
 
+DECLARE
+@AmericanCupTournamentId int = (SELECT TOP 1 [Id] FROM [fixture].[Tournament] WHERE [Name] = 'Copa América Centenario'),
+@CuarterFinalsId int = (SELECT TOP 1 [Id] FROM [fixture].[MatchType] WHERE [Name] = 'Cuartos de Final'),
+@ArgentinaTeamId int = (SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Argentina'),
+@VenezuelaTeamId int = (SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Venezuela'),
+@USATeamId int = (SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'USA'),
+@EcuadorTeamId int = (SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Ecuador'),
+@PeruTeamId int = (SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Perú'),
+@ColombiaTeamId int = (SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Colombia'),
+@MexicoTeamId int = (SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'México'),
+@ChileTeamId int = (SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Chile');
 
 IF NOT EXISTS(SELECT 1 FROM [fixture].[Match])
 BEGIN
 	INSERT INTO [fixture].[Match] ([TournamentId], [TypeId], [HomeTeamId], [AwayTeamId], [HomeTeamGoals], [AwayTeamGoals], [Date])
 	VALUES(
-		(SELECT TOP 1 [Id] FROM [fixture].[Tournament] WHERE [Name] = 'Copa América Centenario'),
-		(SELECT TOP 1 [Id] FROM [fixture].[MatchType] WHERE [Name] = 'Cuartos de Final'),
-		(SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Argentina'),
-		(SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Venezuela'),
+		@AmericanCupTournamentId,
+		@CuarterFinalsId,
+		@ArgentinaTeamId,
+		@VenezuelaTeamId,
 		4,
 		1,
 		'2016-07-04'
 	)
 	INSERT INTO [fixture].[Match] ([TournamentId], [TypeId], [HomeTeamId], [AwayTeamId], [HomeTeamGoals], [AwayTeamGoals], [Date])
 	VALUES(
-		(SELECT TOP 1 [Id] FROM [fixture].[Tournament] WHERE [Name] = 'Copa América Centenario'),
-		(SELECT TOP 1 [Id] FROM [fixture].[MatchType] WHERE [Name] = 'Cuartos de Final'),
-		(SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'USA'),
-		(SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Ecuador'),
+		@AmericanCupTournamentId,
+		@CuarterFinalsId,
+		@USATeamId,
+		@EcuadorTeamId,
 		4,
 		1,
 		'2016-07-04'
 	)
 	INSERT INTO [fixture].[Match] ([TournamentId], [TypeId], [HomeTeamId], [AwayTeamId], [HomeTeamGoals], [AwayTeamGoals], [Date])
 	VALUES(
-		(SELECT TOP 1 [Id] FROM [fixture].[Tournament] WHERE [Name] = 'Copa América Centenario'),
-		(SELECT TOP 1 [Id] FROM [fixture].[MatchType] WHERE [Name] = 'Cuartos de Final'),
-		(SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Perú'),
-		(SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Colombia'),
+		@AmericanCupTournamentId,
+		@CuarterFinalsId,
+		@PeruTeamId,
+		@ColombiaTeamId,
 		4,
 		1,
 		'2016-07-04'
 	)
 	INSERT INTO [fixture].[Match] ([TournamentId], [TypeId], [HomeTeamId], [AwayTeamId], [HomeTeamGoals], [AwayTeamGoals], [Date])
 	VALUES(
-		(SELECT TOP 1 [Id] FROM [fixture].[Tournament] WHERE [Name] = 'Copa América Centenario'),
-		(SELECT TOP 1 [Id] FROM [fixture].[MatchType] WHERE [Name] = 'Cuartos de Final'),
-		(SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'México'),
-		(SELECT TOP 1 [Id] FROM [fixture].[Team] WHERE [Name] = 'Chile'),
+		@AmericanCupTournamentId,
+		@CuarterFinalsId,
+		@MexicoTeamId,
+		@ChileTeamId,
 		4,
 		1,
 		'2016-07-04'
-	)	
+	)
 END
 GO
